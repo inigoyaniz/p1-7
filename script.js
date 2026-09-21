172c2c5417401c1bbcd9825e96ebe8a39c3f7e02
inigoyaniz <inigoyanizibanez@gmail.com>
2026-09-21 18:41:18 +0200

    Ano y director

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-alert("alerta la puerta esta abierta");
-
+const boton = document.getElementById('btnBuscar');
+const ano = document.getElementById('anoPelicula');
+const director = document.getElementById('directorPelicula');
+boton.addEventListener('click',async function(evento) {
+    const input = document.getElementById('inputTitulo');
+    const respuesta =  await fetch(`https://omdbapi.com/?t=${input.value}&apikey=a38147e8`);
+    const datos = await respuesta.json();
+    ano.textContent = datos["Year"];
+    director.textContent = datos["Director"];
+})
